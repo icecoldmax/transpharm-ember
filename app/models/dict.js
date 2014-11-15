8766bee13f59fdcd3d294c444d771ab19d20fa72
David Nettle <david.nettle@gmail.com>
2014-11-15 15:01:31 +1100

    feat(fixtures): Get fixtures showing.

---
+++ app/models/dict.js
@@ -0,0 +1,25 @@
+import DS from "ember-data";
+
+var Dict = DS.Model.extend({
+  eng: DS.attr("string"),
+  jap: DS.attr("string"),
+  say: DS.attr("string")
+});
+
+export default Dict.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      eng: "Hello",
+      jap: "こんにちは",
+      say: "konnichiwa"
+
+    },
+    {
+      id: 2,
+      eng: "Goodbye",
+      jap: "さようなら",
+      say: "sayounara"
+    }
+  ]
+});
