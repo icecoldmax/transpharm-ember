244cefcd3bfa7423ae270675d4254bfafbd61b2e
David Nettle <david.nettle@gmail.com>
2014-11-16 11:38:39 +0800

    feat(sweet-alert): Use sweet alert.

--- app/controllers/dict/new.js
+++ app/controllers/dict/new.js
@@ -3,8 +3,15 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
   actions: {
     createWord: function() {
+      var that = this;
       var word = this.store.createRecord("dict", this.get("fields"));
-      word.save();
+      word.save().then(function() {
+        swal("Word saved!", "", "success");
+        that.set("fields", {});
+      },
+      function() {
+        swal("Word didn't save :(", "Wish I could tell you why...", "error");
+      });
     }
   }
 });
